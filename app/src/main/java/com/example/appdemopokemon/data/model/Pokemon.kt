8008e308840c5fb1b04191aeef6c48885ed4a1cb
Andabero5 <adbeltranr@correo.udistrital.edu.co>
2022-04-07 17:39:19 -0500

    data class

---
+++ app/src/main/java/com/example/appdemopokemon/data/model/Pokemon.kt
@@ -0,0 +1,14 @@
+package com.example.appdemopokemon.data.model
+
+data class Pokemon(
+    val abilities: List<Ability> = listOf(),
+    val height: Int = 0,
+    val name: String = "",
+    val sprites: List<Sprite> = listOf(),
+    val types: List<Type> = listOf(),
+    val weight: Int = 0
+)
+
+data class Ability(val name: String = "")
+data class Sprite(val front_default: String = "")
+data class Type(val names: String = "")
